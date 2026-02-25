840a8e75a91ee59523ba3d2977f23c2896220f0d
dk0171-create <dk0171@srmist.edu.in>
2026-02-25 10:53:56 +0530

    UC2: Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("welcome to Palindrome");
+    public static void main() {
+        String str ="madam";
+        boolean ispal =true;
+        for (int i=0;i<str.length()/2;i++){
+            if (str.charAt(i) != str.charAt(str.length()-1-i)){
+                ispal = false;
+                break;
+            }
+        }
+        if(ispal){
+            System.out.println(str+" is palindrone");
+        }
+        else{
+            System.out.println(str+" is not a palindrone");
+        }
+
     }
 }
