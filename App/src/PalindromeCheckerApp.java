1c70d08ebdffed4a4c5f641842ae0aec43d02a78
Vishnu Reddy <mm1475@srmist.edu.in>
2026-03-12 19:30:03 +0530

    UC1: Application Entry & Welcome Message

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,33 +1,5 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        String original = "level";
-
-        String reversed = "";
-
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
-        }
-
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a Palindrome.");
-        } else {
-            System.out.println(original + " is NOT a Palindrome.");
-        }
-    public static void main() {
-        String str ="madam";
-        boolean ispal =true;
-        for (int i=0;i<str.length()/2;i++){
-            if (str.charAt(i) != str.charAt(str.length()-1-i)){
-                ispal = false;
-                break;
-            }
-        }
-        if(ispal){
-            System.out.println(str+" is palindrone");
-        }
-        else{
-            System.out.println(str+" is not a palindrone");
-        }
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome");
     }
 }
